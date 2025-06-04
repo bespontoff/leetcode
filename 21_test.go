f6b888aa1c76a85908d716f7775c559cdaee729f
bespontoff <bespontoff@gmail.com>
2025-06-04 21:29:35 +0300

    21. Merge Two Sorted Lists

---
+++ 21_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_21(t *testing.T) {
+	t.Log("21. Merge Two Sorted Lists https://leetcode.com/problems/merge-two-sorted-lists/description/")
+	var testcases = []struct {
+		text  string
+		list1 *ListNode
+		list2 *ListNode
+		want  *ListNode
+	}{
+		{"Example 1:", &ListNode{1, &ListNode{2, &ListNode{4, nil}}},
+			&ListNode{1, &ListNode{3, &ListNode{Val: 4}}},
+			&ListNode{1, &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{4, nil}}}}}}},
+		{"Example 2:", &ListNode{}, &ListNode{}, &ListNode{}},
+		{"Example 3:", &ListNode{}, &ListNode{0, nil}, &ListNode{0, nil}},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.text, func(t *testing.T) {
+			result := mergeTwoLists(tc.list1, tc.list2)
+			assert.Equal(t, tc.want, result)
+		})
+	}
+}
