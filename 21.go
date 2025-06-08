04ff69ca7beea8a1c57c37710ecc551bf7d327b0
bespontoff <bespontoff@gmail.com>
2025-06-08 11:46:49 +0300

    21. with method

--- 21.go
+++ 21.go
@@ -7,15 +7,47 @@ type ListNode struct {
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	var result *ListNode
-	if list1 == (&ListNode{}) && list2 == (&ListNode{}) {
-		return &ListNode{}
+	if list1 == nil && list2 == nil {
+		return nil
 	}
-	if list1 == (&ListNode{}) {
+	if list1 == nil {
 		return list2
 	}
-	if list2 == (&ListNode{}) {
+	if list2 == nil {
 		return list1
 	}
+	for list1 != nil && list2 != nil {
+		if list1 == nil {
+			result.insert(list2)
+			list2 = list2.Next
+			continue
+		}
+		if list2 == nil {
+			result.insert(list1)
+			list1 = list1.Next
+			continue
+		}
+		if list1.Val == list2.Val {
+			result.insert(list1)
+			list1 = list1.Next
+			result.insert(list2)
+			list2 = list2.Next
+		} else if list1.Val < list2.Val {
+			result.insert(list1)
+			list1 = list1.Next
+		} else {
+			result.insert(list2)
+			list2 = list2.Next
+		}
 
+	}
 	return result
+}
+
+func (ln *ListNode) insert(n *ListNode) {
+	current := ln
+	for current != nil {
+		current = current.Next
+	}
+	current = n
 }
