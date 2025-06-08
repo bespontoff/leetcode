72c45e8ddd6a64f7f352931187e5915ceaea87dd
bespontoff <bespontoff@gmail.com>
2025-06-08 18:15:25 +0300

    3 faster

--- 3.go
+++ 3.go
@@ -1,32 +1,30 @@
 package main
 
-import "sync"
-
 func lengthOfLongestSubstring(s string) int {
-	longest := 0
-	wg := sync.WaitGroup{}
+	subs := make(map[string]struct{})
 	for subLength := 1; subLength <= len(s); subLength++ {
 		for begin := 0; begin+subLength <= len(s); begin++ {
 			end := begin + subLength
-			wg.Add(1)
-			go func(sub string) {
-				m := make(map[int32]struct{})
-				uniq := true
-				for _, char := range sub {
-					if _, ok := m[char]; !ok {
-						m[char] = struct{}{}
-					} else {
-						uniq = false
-						break
-					}
-				}
-				if uniq && len(sub) > longest {
-					longest = len(sub)
-				}
-				wg.Done()
-			}(s[begin:end])
+			if _, ok := subs[s[begin:end]]; !ok {
+				subs[s[begin:end]] = struct{}{}
+			}
 		}
 	}
-	wg.Wait()
+	longest := 0
+	for sub := range subs {
+		m := make(map[int32]struct{})
+		uniq := true
+		for _, char := range sub {
+			if _, ok := m[char]; !ok {
+				m[char] = struct{}{}
+			} else {
+				uniq = false
+				break
+			}
+		}
+		if uniq && len(sub) > longest {
+			longest = len(sub)
+		}
+	}
 	return longest
 }
